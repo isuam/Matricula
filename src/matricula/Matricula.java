efec2dd4434d6e2407caf9fde139a4ee2ccaff23
isuam <monsebarahona18@gmail.com>
2020-04-08 13:40:47 -0600

    Tarea #9

---
+++ src/matricula/Matricula.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matricula;
+
+/**
+ *
+ * @author Monse
+ */
+public class Matricula {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        vista Vista=new vista();
+        Vista.setVisible(true);
+    }
+    
+}
